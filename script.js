e67e323c93d9d3d88b9a6965d63a47ac8d8b6510
DreamySuhail <140181169+DashingSuhail@users.noreply.github.com>
2023-11-28 14:24:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+function Click(){
+    let Sidenav = document.getElementById("Sidebar");
+    if(Sidenav.style.display === "none"){
+      Sidenav.style.display = "block";
+    }else{
+      Sidenav.style.display = "none";
+    }
+  }
+
+
+  let slideIndex = 0;
+showSlides();
+function showSlides(){
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for ( i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides , 2000);
+}
